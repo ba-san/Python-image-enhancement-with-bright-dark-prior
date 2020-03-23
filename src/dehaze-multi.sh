d208429293ef1eff8f30e2f02b9a9ba2ad50f9e8
ba-san <ohbadai_ybb_ne_jp@yahoo.co.jp>
2020-03-23 10:02:08 +0900

    first version

---
+++ src/dehaze-multi.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash/
+
+workon WRN
+
+files="/home/daisuke/Workplace/Python-image-enhancement-with-bright-dark-prior/crowd_night_trial/*"
+
+for filepath in $files; do
+  faname_ext="${filepath##*/}"
+  echo $faname_ext
+  echo '#########################'
+
+  sed -i -e '160,160d' dehaze.py
+  sed -i '160i \    src = '\"$faname_ext\" dehaze.py
+  
+  python2 dehaze.py
+
+done
